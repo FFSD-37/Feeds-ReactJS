5e0caa56148fdac44d9a2410efc35bb614cf3237
amruthavakadani2005-a11y <amruthavakadani2005@gmail.com>
2026-01-30 10:41:09 +0530

    added admin.js

---
+++ server/models/admin.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+export default Admin;
